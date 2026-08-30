add23cff15b7c1cad13a5fa7c16d23b9cc375fee
AbhijithReddyk <abhijithkandala@gmail.com>
2026-08-30 23:21:58 +0530

    feat: Intialize an LRUCache object.

--- hello_world.go
+++ hello_world.go
@@ -18,6 +18,21 @@ type LRUCache struct {
 	tail *Node
 }
 
+func newLRUCache(capacity int) *LRUCache {
+	head := &Node{}
+	tail := &Node{}
+
+	head.next = tail
+	tail.prev = head
+
+	return &LRUCache{
+		capacity: capacity,
+		cache:    make(map[string]*Node, capacity),
+		head:     head,
+		tail:     tail,
+	}
+}
+
 func main() {
 	m := make([]int, 5)
 	fmt.Println(m)
